a3332c744e506ff104e7f7c4d5d9325e54ffa54c
Jay Chan <56701394+chansome@users.noreply.github.com>
2024-06-28 11:56:17 +0100

    Create Get-IntuneDevice.ps1

---
+++ Get-IntuneDevice.ps1
@@ -0,0 +1,55 @@
+function Get-IntuneDevice {
+    [CmdletBinding(DefaultParameterSetName = 'ByName')]
+    param (
+        [Parameter(Mandatory, ParameterSetName = 'ByName')]
+        [string]$DeviceName,
+        [Parameter(Mandatory, ParameterSetName = 'BySerial')]
+        [string]$SerialNumber,
+        [Parameter(Mandatory, ParameterSetName = 'ByUPN')]
+        [string]$UserPrincipalName      
+    )
+
+    switch ($true) {
+        { $PSCmdlet.MyInvocation.BoundParameters.Values.EndsWith('*') -and $PSCmdlet.MyInvocation.BoundParameters.Values.StartsWith('*') } {
+            $filter = "`$filter=contains($($PSCmdlet.MyInvocation.BoundParameters.Keys),'$($PSCmdlet.MyInvocation.BoundParameters.Values -replace '\*')')"
+            continue
+        }
+        { $PSCmdlet.MyInvocation.BoundParameters.Values.EndsWith('*') } { 
+            $filter = "`$filter=startswith($($PSCmdlet.MyInvocation.BoundParameters.Keys),'$($PSCmdlet.MyInvocation.BoundParameters.Values -replace '\*')')"
+            continue
+        }
+        { $PSCmdlet.MyInvocation.BoundParameters.Values.StartsWith('*') } {
+            $filter = "`$filter=endswith($($PSCmdlet.MyInvocation.BoundParameters.Keys),'$($PSCmdlet.MyInvocation.BoundParameters.Values -replace '\*')')"
+            continue
+        }
+        { $null -ne $PSCmdlet.MyInvocation.BoundParameters.Values } {
+            $filter = "`$filter=$($PSCmdlet.MyInvocation.BoundParameters.Keys) eq '$($PSCmdlet.MyInvocation.BoundParameters.Values)'"
+        }
+    }
+    
+    $intuneDevices = Invoke-MgGraphRequest -Method GET "https://graph.microsoft.com/beta/deviceManagement/managedDevices?$filter"
+
+    switch ($true) {
+        { $intuneDevices.value.Count -gt 0 } {
+            Write-Host "$($intuneDevices.value.Count) found: $($intuneDevices.value.deviceName)"
+            
+            $retVal = $intuneDevices.value
+
+            continue
+        }
+        { $intuneDevices.value.Count -eq 0 } {
+            Write-Host "No Intune devices found with $($PSCmdlet.MyInvocation.BoundParameters.Keys) of '$($PSCmdlet.MyInvocation.BoundParameters.Values)'"
+
+            $retVal = 'No device(s) found'
+
+            continue
+        }
+        # { $loopCounter -ge 10 } {
+        #    Write-Host "Retry limit reached $loopCounter with no success"
+        #
+        #    $retVal = 'Retry limit reached'
+        # }
+    }
+    
+    return $retVal
+}
