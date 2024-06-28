16613245bb30c206114acb6932a8a6af7d6e7397
Jay Chan <56701394+chansome@users.noreply.github.com>
2024-06-28 11:37:47 +0100

    Added Get-AuthTokenUsingCert

--- graph.psm1
+++ graph.psm1
@@ -1,3 +1,40 @@
+function Get-AuthTokenUsingCert {
+    [CmdletBinding()]
+    param (
+        [Parameter()]
+        [string]$TenantId,
+        [Parameter()]
+        [string]$ClientId,        
+        [Parameter()]
+        [string]$CertThumbprint
+    )
+
+    try {
+        $cert = Get-ChildItem Cert:\LocalMachine\My\$CertThumbprint -ErrorAction Stop
+    }
+    catch {
+        $message = "Unable to get certificate: $($_.Exception.Message)"
+        throw $message
+    }
+
+    $connectionDetails = @{
+        TenantId           = $TenantId
+        ClientId           = $ClientId
+        ClientCertificate  = $cert
+        AzureCloudInstance = 1
+    }
+
+    $token = Get-MsalToken @connectionDetails
+
+    $authHeader = @{
+        'Content-Type'  = 'application/json'
+        'Authorization' = $token.CreateAuthorizationHeader()
+        'ExpiresOn'     = $token.ExpiresOn
+    }
+
+    return $authHeader
+}
+
 function Get-IntuneDevice {
     [CmdletBinding(DefaultParameterSetName = 'ByName')]
     param (
